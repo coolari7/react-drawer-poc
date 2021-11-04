081545189b1fec18ea68ecd802e7af3de270d4c9
Arijit Chowdhury <coolari7@gmail.com>
2021-11-04 19:51:16 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import React from "react";
+import { Backdrop } from "./Backdrop";
+import { Button } from "./Button";
+import { Drawer } from "./Drawer";
+import { Panel } from "./Panel";
+
 export default function App() {
+  const [open, setOpen] = React.useState(false);
+
+  const [open2, setOpen2] = React.useState(false);
+
   return (
     <div className="m-2">
-      <h1 className="font-bold">App Works</h1>
+      <div className="float-right">
+        <Button onClick={() => setOpen(true)}>Open Drawer</Button>
+      </div>
+
+      <div
+        style={{ width: "200px", height: "200px" }}
+        className="ml-36 border-2 border-gray-900 relative"
+      >
+        <div className="float-right p-2">
+          <Button onClick={() => setOpen2(true)}>Open</Button>
+        </div>
+        <Backdrop open={open2} onClose={() => setOpen2(false)} />
+        <Panel open={open2}>
+          <Button onClick={() => setOpen2(false)}>Close</Button>
+        </Panel>
+      </div>
+
+      <Drawer open={open} timeout={250} onClose={() => setOpen(false)}>
+        <h1 className="text-6xl font-bold text-gray-500">Content</h1>
+        <Button onClick={() => setOpen(false)}>Close Drawer</Button>
+      </Drawer>
     </div>
   );
 }
